2218b32c1a7e9955408da01ee26957bd7400fd62
yutakaki <takaki.yu.dev@gmail.com>
2022-02-21 13:05:33 +0900

    add lazy loading in image

--- src/components/project.js
+++ src/components/project.js
@@ -5,7 +5,7 @@ const Project = ({ proj }) => {
   return (
     <Link to={`/project/${proj.name}`} className="project">
       <figure>
-        <img src={require(`../assets/${proj.mainPic}`)} alt="preview" />
+        <img src={require(`../assets/${proj.mainPic}`)} alt="preview" loading="lazy" />
         <div className="projectName">
           <h2>{proj.name}</h2>
         </div>
