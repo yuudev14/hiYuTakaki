b3faabbc5e8b9aedab5cfce399c58ecd25504555
yutakaki <takaki.yu.dev@gmail.com>
2022-10-15 12:52:20 +0900

    animation when opening the bot

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import { AiFillRobot } from "react-icons/ai";
 import { TbArrowsMinimize } from "react-icons/tb";
 const Bot = () => {
+  const botBtnRef = useRef();
+
+  const openBotHandler = () => {
+    botBtnRef.current.classList.remove("inactive");
+    botBtnRef.current.classList.add("active");
+  };
+  const closeBotHandler = (e) => {
+    e.stopPropagation();
+    botBtnRef.current.classList.remove("active");
+    botBtnRef.current.classList.add("inactive");
+  };
   return (
-    <div className="fixed bottom-7 right-7 z-10">
-      <div className="absolute bottom-1 right-1 bg-[#16bbac] p-5 rounded-full shadow-md shadow-[#16bbac] transition-all hover:scale-105 cursor-pointer z-50">
-        <AiFillRobot size={40} />
-      </div>
-      <div className="bg-white h-[500px] w-[400px] absolute bottom-8 right-10 z-40 shadow-2xl border-gray-200 border-2 rounded-md flex flex-col">
-        <div className="h-16 w-full bg-[#16bbac] flex justify-end items-center p-5">
-          <TbArrowsMinimize size={20} />
+    <div className="bot-container">
+      <div
+        ref={botBtnRef}
+        onClick={openBotHandler}
+        className="bot-msg-box inactive shadow-lg border-2 border-zinc-200">
+        <div className="bot-header">
+          <AiFillRobot size={40} className="bot-icon" />
+          <TbArrowsMinimize
+            size={20}
+            className="bot-minimize"
+            onClick={closeBotHandler}
+          />
         </div>
-        <div className="flex-1 w-full p-5"></div>
-        <div className="h-16 w-full flex border-gray-200 border-t-2">
-          <TbArrowsMinimize size={20} />
+        <div className="bot-text-content"></div>
+        <div className="bot-textbox-container">
+          <p contentEditable>
+            Consectetur nostrud occaecat irure adipisicing ut irure dolor quis
+            reprehenderit elit occaecat fugiat cillum. Eu mollit amet veniam
+            incididunt duis in ipsum nostrud labore sint ipsum amet qui
+            consectetur. Nisi ipsum magna deserunt cillum.
+          </p>
         </div>
       </div>
     </div>
